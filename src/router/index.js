aa5c95b53e112411411129322afc41aae6ff9d25
DavidAGB <104640308+DavidAGB@users.noreply.github.com>
2022-05-19 11:50:42 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,8 +12,7 @@ import Dashboard from "../views/dashboard/Dashboard.vue";
 
 
 const router = createRouter({
-    history: createWebHistory(
-        import.meta.env.BASE_URL),
+    history: createWebHistory('vue'),
     routes: [{
             path: "/",
             name: "home",
